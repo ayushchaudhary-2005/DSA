34ac0f52630aae4eb082cbfe0f96ae9920c2bf17
ayushchaudhary-2005 <ayushlathar18@gmail.com>
2026-01-25 02:18:13 +0530

    Time: 2 ms (53.51%) | Memory: 43.5 MB (47.56%) - LeetSync

--- 2337-remove-digit-from-number-to-maximize-result/remove-digit-from-number-to-maximize-result.java
+++ 2337-remove-digit-from-number-to-maximize-result/remove-digit-from-number-to-maximize-result.java
@@ -1,4 +1,23 @@
 class Solution {
+    String ans="";
+    public void find(String number,char digit,int operation,int idx){
+        if(idx==number.length()){
+            return;
+        }
+        if(number.charAt(idx)==digit){
+            String remStr=number.substring(0,idx)+number.substring(idx+1,number.length());
+            if(ans.equals("")){
+                ans=remStr;
+            }
+            else{
+                if(ans.compareTo(remStr)<0){
+                    ans=remStr;
+                }
+            }
+
+        }
+        find(number,digit,operation,idx+1);
+    }
     public String removeDigit(String number, char digit) {
         int c=0;
         int idx=-1;
@@ -14,30 +33,9 @@ class Solution {
             return sb.toString();
         }
         else{
-            idx=-1;
-            for(int i=0;i<number.length()-1;i++){
-                if(number.charAt(i)==digit && number.charAt(i)<number.charAt(i+1)){
-                    idx=i;
-                    break;
-                }
-            }
-            if(idx!=-1){
-                StringBuilder sb=new StringBuilder(number);
-                sb.deleteCharAt(idx);
-                return sb.toString();
-            }
-            else{
-                for(int i=number.length()-1;i>=0;i--){
-                    if(number.charAt(i)==digit){
-                        idx=i;
-                        break;
-                    }
-                }
-                StringBuilder sb=new StringBuilder(number);
-                sb.deleteCharAt(idx);
-                return sb.toString();
-            }
-        }
+            find(number,digit,0,0);
+            return ans;
+            
         
     }
-}
+}}
