424072b3629fe788827710a8c7e84a593c2e8836
ayushchaudhary-2005 <ayushlathar18@gmail.com>
2026-03-19 12:34:26 +0530

    Time: 0 ms (100.00%) | Memory: 43 MB (13.49%) - LeetSync

--- 198-house-robber/house-robber.java
+++ 198-house-robber/house-robber.java
@@ -1,14 +1,14 @@
 class Solution {
     public int find(int arr[],int idx,int dp[]){
-        if(arr.length<=idx){
+        if(idx>=arr.length){
             return 0;
         }
         if(dp[idx]!=-1){
             return dp[idx];
         }
-        int steal=arr[idx]+find(arr,idx+2,dp);
+        int take=arr[idx]+find(arr,idx+2,dp);
         int skip=find(arr,idx+1,dp);
-        return dp[idx]=Math.max(steal,skip);
+        return dp[idx]=Math.max(take,skip);
     }
     public int rob(int[] arr) {
         int dp[]=new int[arr.length];
