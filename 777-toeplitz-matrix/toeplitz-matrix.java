de14cb80b6df590ee00476c2cd3970a3e1f8630a
ayushchaudhary-2005 <ayushlathar18@gmail.com>
2026-04-12 17:37:00 +0530

    Time: 0 ms (100.00%) | Memory: 46.6 MB (34.43%) - LeetSync

---
+++ 777-toeplitz-matrix/toeplitz-matrix.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean isToeplitzMatrix(int[][] arr) {
+        for(int i=0;i<arr.length-1;i++){
+            for(int j=0;j<arr[0].length-1;j++){
+                if(arr[i][j]!=arr[i+1][j+1]){
+                    return  false;
+                }
+            }
+        }
+        return true;
+        
+    }
+}
