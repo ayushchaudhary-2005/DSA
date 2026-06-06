bebd24503394772f639caa345f5108ad126df673
ayushchaudhary-2005 <ayushlathar18@gmail.com>
2026-06-06 13:18:00 +0530

    Time: 508 ms (46.18%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1278-product-price-at-a-given-date/product-price-at-a-given-date.sql
@@ -0,0 +1,2 @@
+SELECT c.product_id,COALESCE(t.price,10) AS price FROM (SELECT DISTINCT product_id FROM Products ) AS c LEFT JOIN (SELECT product_id,new_price as price FROM Products 
+WHERE (product_id,change_date) IN (SELECT product_id,MAX(change_date) as change_date FROM Products WHERE change_date<='2019-08-16' GROUP BY product_id)) as t ON c.product_id=t.product_id;
