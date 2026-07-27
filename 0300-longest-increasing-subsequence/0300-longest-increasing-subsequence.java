7c60970ce9768efe975a06675f596d161fdd0f70
ayushchaudhary-2005 <ayushlathar18@gmail.com>
2026-07-27 17:35:20 +0530

    Time: 37 ms (72.07%), Space: 46 MB (70.45%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
@@ -1,29 +1,18 @@
 class Solution {
-    public int find(int arr[],int idx,int prevIdx,int dp[][]){
-        if(idx==arr.length){
-            return 0;
-        }
-        if(prevIdx>=0 && dp[idx][prevIdx]!=-1){
-            return dp[idx][prevIdx];
-        }
-        int take=0;
-        if(prevIdx==-1 || arr[prevIdx]<arr[idx]){
-            take=1+find(arr,idx+1,idx,dp);
-        }
-        int notTake=find(arr,idx+1,prevIdx,dp);
-        if(prevIdx>=0){
-            dp[idx][prevIdx]=Math.max(take,notTake);
-        }
-        return Math.max(take,notTake);
-
-    }
     public int lengthOfLIS(int[] arr) {
-        int dp[][]=new int[2501][2501];
-        
-        for(int i=0;i<2501;i++){
-            Arrays.fill(dp[i],-1);
+        int ans[]=new int[arr.length];
+        Arrays.fill(ans,1);
+        int max=0;
+        for(int i=0;i<arr.length;i++){
+            for(int j=0;j<i;j++){
+                if(arr[j]<arr[i]){
+                    ans[i]=Math.max(ans[i],1+ans[j]);
+
+                }
+            }
+            max=Math.max(max,ans[i]);
         }
-        return find(arr,0,-1,dp);
+        return max;
     
     }
 }
